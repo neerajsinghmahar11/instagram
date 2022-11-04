b1d35b4e805f640f9bc23598a5d5cb5b97f19dcd
neerajsinghmahar11 <neeraj.singh.mahar11@gmail.com>
2022-11-04 23:42:13 +0530

    pages update

--- src/home.js
+++ src/home.js
@@ -12,7 +12,30 @@ let post_div=document.getElementById("posts");
 
 const getdata=async()=>{
     let res=await fetch(`https://glacial-eyrie-24074.herokuapp.com/posts`);
-    let data=await res.json();
-    append(data,post_div);  
+    let data=await res.json();  
+    createButton(data.length,2)
 }
 getdata();
+const getPaginatedData=async(clicked_button,limit)=>{
+    let res=await fetch(`https://glacial-eyrie-24074.herokuapp.com/posts?_page=${clicked_button}&_limit=${limit}`);
+    let data=await res.json();
+    append(data,post_div);
+}
+getPaginatedData(1,2);
+
+let buttons_div=document.getElementById("buttons");
+const createButton=(total_images,image_per_page)=>{
+    const buttons=Math.ceil(total_images/image_per_page);
+
+    for(let i=1;i<=buttons;i++){
+        let btn=document.createElement("button");
+
+        btn.id=i;
+        btn.innerText=i;
+        btn.onclick=()=>{
+            getPaginatedData(i,2);
+        };
+
+        buttons_div.append(btn);
+    };
+};
